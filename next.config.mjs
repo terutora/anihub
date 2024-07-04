922781123529b8afc35c284594d4982a331ba369
terutora <tachi877teru@gmail.com>
2024-07-04 13:25:24 +0900

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['api.annict.com', 'cal.syoboi.jp'], // APIから画像を取得する場合に必要
+    },
+  }
+  
+  module.exports = nextConfig
